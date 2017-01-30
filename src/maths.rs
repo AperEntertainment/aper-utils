96d7b27fd2948e9cd2c81ba929bef71e2974607b
adri326 <adrien.burgun@orange.fr>
2017-01-30 10:46:44 +0100

    Oops, set the min() and max() to public

--- src/maths.rs
+++ src/maths.rs
@@ -1,5 +1,8 @@
-fn min(a: f32, b: f32) -> f32 {
+pub fn min(a: f32, b: f32) -> f32 {
     return if a > b { b } else { a };
+}
+pub fn min(a: f32, b: f32) -> f32 {
+    return if a < b { b } else { a };
 }
 #[cfg(test)]
 mod tests {

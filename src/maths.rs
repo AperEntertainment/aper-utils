af7fb9c10d78df30660c32eeaad02ef79de78235
adri326 <adrien.burgun@orange.fr>
2017-01-30 10:54:40 +0100

    Fixed my horrible code :)

--- src/maths.rs
+++ src/maths.rs
@@ -1,10 +1,8 @@
-pub mod maths {
-    pub fn min(a: f32, b: f32) -> f32 {
-        return if a > b { b } else { a };
-    }
-    pub fn min(a: f32, b: f32) -> f32 {
-        return if a < b { b } else { a };
-    }
+pub fn min(a: f32, b: f32) -> f32 {
+    return if a > b { b } else { a };
+}
+pub fn max(a: f32, b: f32) -> f32 {
+    return if a < b { b } else { a };
 }
 #[cfg(test)]
 mod tests {

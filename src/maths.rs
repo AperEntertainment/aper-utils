c1240caeb9691659e612e519e0430b62439c3a40
adri326 <adrien.burgun@orange.fr>
2017-01-30 14:39:16 +0100

    Made both functions generic

--- src/maths.rs
+++ src/maths.rs
@@ -1,13 +1,16 @@
-pub fn min(a: f32, b: f32) -> f32 {
-    return if a > b { b } else { a };
-}
-pub fn max(a: f32, b: f32) -> f32 {
+use std::cmp::PartialOrd;
+
+pub fn min<T>(a: T, b: T) -> T where T: PartialOrd {
     return if a < b { b } else { a };
+}
+pub fn max<T>(a: T, b: T) -> T where T: PartialOrd {
+    return if a > b { b } else { a };
 }
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        println!("{}", super::min(56.0 as f32, 23.0 as f32));
+        println!("r: {:?}", super::min(56.0 as f32, 23.0 as f32));
+        println!("r: {:?}", super::max(56.0 as f32, 23.0 as f32));
     }
 }

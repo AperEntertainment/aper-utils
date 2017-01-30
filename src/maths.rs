02f96df887423b7daa23a6af79ca838e4c3311be
adri326 <adrien.burgun@orange.fr>
2017-01-30 14:52:14 +0100

    Inverted the min and the max function (how good am I to maths :)

--- src/maths.rs
+++ src/maths.rs
@@ -1,10 +1,10 @@
 use std::cmp::PartialOrd;
 
 pub fn min<T>(a: T, b: T) -> T where T: PartialOrd {
-    return if a < b { b } else { a };
+    return if a > b { b } else { a };
 }
 pub fn max<T>(a: T, b: T) -> T where T: PartialOrd {
-    return if a > b { b } else { a };
+    return if a < b { b } else { a };
 }
 #[cfg(test)]
 mod tests {
@@ -12,5 +12,6 @@ mod tests {
     fn it_works() {
         println!("r: {:?}", super::min(56.0 as f32, 23.0 as f32));
         println!("r: {:?}", super::max(56.0 as f32, 23.0 as f32));
+        println!("r: {:?}", super::max(1280.5, 1253.0));
     }
 }

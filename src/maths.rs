8d2832167d5dc252c1195a0826feb226281ff503
adri326 <adrien.burgun@orange.fr>
2017-01-30 10:31:56 +0100

    Initial commit (min() and max() for f32)

---
+++ src/maths.rs
@@ -0,0 +1,10 @@
+fn min(a: f32, b: f32) -> f32 {
+    return if a > b { b } else { a };
+}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        println!("{}", super::min(56.0 as f32, 23.0 as f32));
+    }
+}
